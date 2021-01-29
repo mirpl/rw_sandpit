25cea7fac88c487a9ef144f507e757dc15d5671b
psobieranski <psobieranski@mac-mir.home>
2021-01-29 12:23:23 +0100

    blabla

--- src/test/java/Setup.java
+++ src/test/java/Setup.java
@@ -1,15 +1,23 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class Setup {
 
     @Test
     public static void setupPage() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-size=1400,600");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--single-process");
+        options.addArguments("--use-gl=swiftshader");
+        options.addArguments("--no-zygote");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.onet.pl");
         driver.quit();
     }
-
 }
